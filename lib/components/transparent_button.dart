1024ec540e85fafcafb6d3b01b12da75db232748
Kyaw Thu <evanch98@gmail.com>
2022-11-10 22:52:32 +0800

    Minor changes

--- lib/components/transparent_button.dart
+++ lib/components/transparent_button.dart
@@ -2,13 +2,18 @@ import 'package:calculator_flutter/components/transparent_container.dart';
 import 'package:flutter/material.dart';
 
 class TransparentButton extends StatelessWidget {
-  const TransparentButton({Key? key}) : super(key: key);
+  const TransparentButton({
+    Key? key,
+    required this.symbol,
+  }) : super(key: key);
+
+  final String symbol;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      child: const TransparentContainer(
-        child: Text("1"),
+      child: TransparentContainer(
+        child: Text(symbol),
       ),
       onTap: () {},
     );
