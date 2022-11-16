8d2a158247b03d5dc0b1b7b6c94b372cf3aaac8f
Kyaw Thu <evanch98@gmail.com>
2022-11-16 18:26:07 +0800

    Adding kTextStyle constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/material.dart';
 
 const kBackgroundColor = Color(0xFF9879F0);
+
+const kTextStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontSize: 40,
+);
