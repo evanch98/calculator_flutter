3ce992b1bd3aa457ac684b09dd872ff1654116b9
Kyaw Thu <evanch98@gmail.com>
2022-11-17 23:21:53 +0800

    Added comments to explain the codes

--- lib/components/button_row.dart
+++ lib/components/button_row.dart
@@ -2,6 +2,15 @@ import 'package:calculator_flutter/components/transparent_button.dart';
 import 'package:calculator_flutter/constants.dart';
 import 'package:flutter/material.dart';
 
+/*
+ * The ButtonRow class consists of four buttons.
+ * This widget is specifically designed for this project; therefore, it will not
+ * be useful for other projects. In addition, the structure of this class is not
+ * extensible.
+ * There are eight parameters.
+ *  Symbols accept String values.
+ *  Functions accept function.
+ */
 class ButtonRow extends StatelessWidget {
   const ButtonRow({
     Key? key,
@@ -15,53 +24,61 @@ class ButtonRow extends StatelessWidget {
     required this.fourthBtnFunction,
   }) : super(key: key);
 
-  final String firstBtnSymbol;
-  final String secondBtnSymbol;
-  final String thirdBtnSymbol;
-  final String fourthBtnSymbol;
+  final String firstBtnSymbol; // the symbol for the first button
+  final String secondBtnSymbol; // the symbol for the second button
+  final String thirdBtnSymbol; // the symbol for the third button
+  final String fourthBtnSymbol; // the symbol for the fourth button
 
-  final Function? firstBtnFunction;
-  final Function? secondBtnFunction;
-  final Function? thirdBtnFunction;
-  final Function? fourthBtnFunction;
+  final Function? firstBtnFunction; // onTap function for the first button
+  final Function? secondBtnFunction; // onTap function for the second button
+  final Function? thirdBtnFunction; // onTap function for the third button
+  final Function? fourthBtnFunction; // onTap function for the fourth button
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
+        // First button - start
         Expanded(
           child: TransparentButton(
             symbol: firstBtnSymbol,
             onTap: firstBtnFunction,
           ),
         ),
+        // First button - end
         const SizedBox(
           width: kGutterSize,
         ),
+        // Second button - start
         Expanded(
           child: TransparentButton(
             symbol: secondBtnSymbol,
             onTap: secondBtnFunction,
           ),
         ),
+        // Second button - end
         const SizedBox(
           width: kGutterSize,
         ),
+        // third button - start
         Expanded(
           child: TransparentButton(
             symbol: thirdBtnSymbol,
             onTap: thirdBtnFunction,
           ),
         ),
+        // third button - end
         const SizedBox(
           width: kGutterSize,
         ),
+        // Fourth button - start
         Expanded(
           child: TransparentButton(
             symbol: fourthBtnSymbol,
             onTap: fourthBtnFunction,
           ),
         ),
+        // Fourth button - end
       ],
     );
   }
