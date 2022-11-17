d19e785ca245399b1a836c80ac8380b9170aa5e4
Kyaw Thu <evanch98@gmail.com>
2022-11-17 23:14:55 +0800

    add new constant values

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 
 const kBackgroundColor = Color(0xFF9879F0);
 
-const kTextStyle = TextStyle(
+const kGutterSize = 3.0;
+
+const kScreenTextStyle = TextStyle(
   color: Colors.white,
   fontWeight: FontWeight.bold,
   fontSize: 40,
+);
+
+const kButtonTextStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontSize: 30,
 );
