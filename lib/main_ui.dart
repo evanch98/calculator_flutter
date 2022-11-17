52cddc719f21e6adc4637aaf7407f35f429ecd67
Kyaw Thu <evanch98@gmail.com>
2022-11-17 23:22:09 +0800

    Updating UI

--- lib/main_ui.dart
+++ lib/main_ui.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '/constants.dart';
+import 'components/button_row.dart';
 import 'components/transparent_container.dart';
 
 class MainUi extends StatefulWidget {
@@ -14,23 +16,113 @@ class _MainUiState extends State<MainUi> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.purpleAccent,
+        backgroundColor: kBackgroundColor,
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: <Widget>[
-              Expanded(
+              const Expanded(
                 child: TransparentContainer(
-                  child: Text(""),
                   size: double.infinity,
+                  radiusSize: 20.0,
+                  topBorder: true,
+                  child: Center(
+                    child: Text(
+                      "12+5",
+                      style: kScreenTextStyle,
+                    ),
+                  ),
                 ),
               ),
               Expanded(
-                child: TransparentContainer(
-                  child: Text(""),
-                  size: double.infinity,
-                ),
                 flex: 2,
+                child: TransparentContainer(
+                  size: double.infinity,
+                  radiusSize: 20.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: TransparentContainer(
+                      size: double.infinity,
+                      radiusSize: 0.0,
+                      child: Column(
+                        children: <Widget>[
+                          Expanded(
+                            child: ButtonRow(
+                              firstBtnSymbol: "AC",
+                              firstBtnFunction: () {},
+                              secondBtnSymbol: "x²",
+                              secondBtnFunction: () {},
+                              thirdBtnSymbol: "=",
+                              thirdBtnFunction: () {},
+                              fourthBtnSymbol: "←",
+                              fourthBtnFunction: () {},
+                            ),
+                          ),
+                          const SizedBox(
+                            height: kGutterSize,
+                          ),
+                          Expanded(
+                            child: ButtonRow(
+                              firstBtnSymbol: "7",
+                              firstBtnFunction: () {},
+                              secondBtnSymbol: "8",
+                              secondBtnFunction: () {},
+                              thirdBtnSymbol: "9",
+                              thirdBtnFunction: () {},
+                              fourthBtnSymbol: "+",
+                              fourthBtnFunction: () {},
+                            ),
+                          ),
+                          const SizedBox(
+                            height: kGutterSize,
+                          ),
+                          Expanded(
+                            child: ButtonRow(
+                              firstBtnSymbol: "4",
+                              firstBtnFunction: () {},
+                              secondBtnSymbol: "5",
+                              secondBtnFunction: () {},
+                              thirdBtnSymbol: "6",
+                              thirdBtnFunction: () {},
+                              fourthBtnSymbol: "–",
+                              fourthBtnFunction: () {},
+                            ),
+                          ),
+                          const SizedBox(
+                            height: kGutterSize,
+                          ),
+                          Expanded(
+                            child: ButtonRow(
+                              firstBtnSymbol: "1",
+                              firstBtnFunction: () {},
+                              secondBtnSymbol: "2",
+                              secondBtnFunction: () {},
+                              thirdBtnSymbol: "3",
+                              thirdBtnFunction: () {},
+                              fourthBtnSymbol: "×",
+                              fourthBtnFunction: () {},
+                            ),
+                          ),
+                          const SizedBox(
+                            height: kGutterSize,
+                          ),
+                          Expanded(
+                            child: ButtonRow(
+                              firstBtnSymbol: "±",
+                              firstBtnFunction: () {},
+                              secondBtnSymbol: "0",
+                              secondBtnFunction: () {},
+                              thirdBtnSymbol: ".",
+                              thirdBtnFunction: () {},
+                              fourthBtnSymbol: "÷",
+                              fourthBtnFunction: () {},
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
