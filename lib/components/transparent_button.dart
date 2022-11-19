fab937d11264d8ec4d40ccc37a843bc912c1b92e
Kyaw Thu <evanch98@gmail.com>
2022-11-20 00:39:19 +0800

    Adding comments

--- lib/components/transparent_button.dart
+++ lib/components/transparent_button.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 
 import 'transparent_container.dart';
 
+/*
+ * The transparent button is simply the TransparentContainer() inside the
+ * InkWell() widget to detect gestures and get splash effect.
+ * There two required parameters to use this widget.
+ * Parameters:
+ *  symbol accepts String values that will display on the button.
+ *  onTap accepts Functions that will be callback.
+ */
 class TransparentButton extends StatelessWidget {
   const TransparentButton({
     Key? key,
@@ -15,20 +23,23 @@ class TransparentButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      child: TransparentContainer(
-        size: double.infinity,
-        radiusSize: 0.0,
-        child: Center(
-          child: Text(
-            symbol,
-            style: kButtonTextStyle,
+    return Material(
+      // Wrapped inside the Material widget to get a splash effect.
+      child: InkWell(
+        child: TransparentContainer(
+          size: double.infinity,
+          radiusSize: 0.0,
+          child: Center(
+            child: Text(
+              symbol,
+              style: kButtonTextStyle,
+            ),
           ),
         ),
+        onTap: () {
+          onTap!();
+        },
       ),
-      onTap: () {
-        onTap!();
-      },
     );
   }
 }
