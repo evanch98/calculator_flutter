233ad76280745f8e2772348157448e83582bece2
Kyaw Thu <evanch98@gmail.com>
2022-11-20 00:34:54 +0800

    Adding comments for the constant variables

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
-//TODO: Add comments
-
+// Image url
 const kNetworkImage =
     "https://images.pexels.com/photos/4022090/pexels-photo-4022090.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
+// Gut between each button
 const kGutterSize = 3.0;
 
+// Text style for the screen
 const kScreenTextStyle = TextStyle(
   color: Colors.white,
   fontWeight: FontWeight.bold,
   fontSize: 40,
 );
 
+// Text style for the button
 const kButtonTextStyle = TextStyle(
   color: Colors.black54,
   fontWeight: FontWeight.bold,
